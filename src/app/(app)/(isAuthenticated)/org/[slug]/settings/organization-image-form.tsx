14aa79dbd7a19a69af42232e5d05a37ea1a41bae
Felipe Fardo <felipe.fardo@hotmail.com.br>
2025-08-29 16:07:40 -0300

    fix: cancel to cancelar in organization image

--- src/app/(app)/(isAuthenticated)/org/[slug]/settings/organization-image-form.tsx
+++ src/app/(app)/(isAuthenticated)/org/[slug]/settings/organization-image-form.tsx
@@ -186,7 +186,7 @@ export function OrganizationImageForm({
               className="gap-2"
             >
               <X className="h-4 w-4" />
-              Cancelar
+              Cancel
             </Button>
           </div>
         )}
